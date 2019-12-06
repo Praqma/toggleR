da0e4808a127391b079b707e3e8442562cdc05b9
Bosse Nyström <bon@praqma.net>
2019-12-06 05:18:45 +0100

    Updates git tag command

--- version.sh
+++ version.sh
@@ -15,7 +15,7 @@ sed -i -e "s/0.0.0.9000/$version/" DESCRIPTION
 echo "R Version: $version"
 
 # are we building an annotated tag
-message=$(git tag -n $version)
+message=$(git tag -ln $version)
 if [ -n "$message" ]; then
   echo ${message#$version} > message.txt
 fi
